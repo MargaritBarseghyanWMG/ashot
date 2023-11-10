411d8c0693b799d0e895579e573e255122316e51
margarit <mak.barseghyan@gmail.com>
2023-11-10 20:35:14 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        // Type casting -> tiperi dzevapoxutyun
+
+        // UpCasting -> javan automate anume ays gortsoxutyuny
+        // Tar = tar _ $
+        int myInt = 19; // 4 byte
+        double myDouble = myInt; // 8 byte//
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myInt == myDouble);
+
+        // byte -> short -> int -> float-> double -> long
+
+        byte myByte = 6;
+        float myFloat = myInt;
+
+        char myChar = 'a';
+        myInt = myChar;
+        System.out.println(myChar);
+        System.out.println(myInt);
+
+
+        // DownCasting
+        myDouble = 17.8;
+        myInt = (int) myDouble;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+
+        myInt = 60;
+        myChar = (char) myInt;
+        System.out.println(myInt);
+        System.out.println(myChar);
+
+    }
+}
