05abf07c3c0d09a18e29b53cf24d8251be5a89fd
margarit <mak.barseghyan@gmail.com>
2023-11-12 20:33:18 +0400

    if statement

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         // UpCasting -> javan automate anume ays gortsoxutyuny
         // Tar = tar _ $
-        int myInt = 19; // 4 byte
+ /*       int myInt = 19; // 4 byte
         double myDouble = myInt; // 8 byte//
         System.out.println(myInt);
         System.out.println(myDouble);
@@ -30,8 +30,40 @@ public class Main {
         myInt = 60;
         myChar = (char) myInt;
         System.out.println(myInt);
-        System.out.println(myChar);
+        System.out.println(myChar);*/
 
-        int tazaBan=7262;
+
+        int a=16;
+        int s=3;
+        if (a/(double)s>7) {
+            System.out.println(a/(double)s);
+            System.out.println(a&s);
+
+        } else if(5*4>100){
+
+            System.out.println(a*s);
+        } else if(4-4==0){
+            System.out.println(4);
+        }
+
+        short d=5;
+        if (d%2==0)
+            System.out.println(0);
+
+        else System.out.println(1);
+
+        // ete tivy 10in patike tpel 10
+        int g=500;
+        if (g%10==0)
+            System.out.println(10);
+
+        //ete tivy 3in bajanelis talise 1 mnacord tpel 1
+        // hakarak depqum tpel 2
+
+        long h=7;
+        if (h%3==1)
+            System.out.println(1);
+        else System.out.println(2);
+
     }
 }
